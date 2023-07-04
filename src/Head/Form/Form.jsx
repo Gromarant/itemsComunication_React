b6ef87f96d5ed4375375a997cf737c64da618928
Mariangelica Rodriguez <gromarant@gmail.com>
2023-07-04 18:14:42 +0200

    Siblings comunication works

--- src/Head/Form/Form.jsx
+++ src/Head/Form/Form.jsx
@@ -6,38 +6,41 @@ const Form = ({ setData }) => {
   const [inputValue, setInputValue] = useState({})
 
   const handleChange = (e) => {
-    e.preventDefault();
-
+    
     setInputValue({
       ...inputValue,
       [e.target.name]: e.target.value,
-      [e.target.email]: e.target.value,
-      [e.target.url]: e.target.value,
-      [e.target.age]: e.target.value,
     })
-};
-
-  const handleClick = () => {
+  }
+  
+  const handleClick = (e) => {
+    e.preventDefault();
     setData(inputValue);
+    setInputValue({
+      name: "",
+      email: "",
+      url: "",
+      age: ""
+    })
   };
 
   return (
     <>
       <form>
         <label htmlFor="name">Name: 
-          <input type="text" />
+          <input type="text" name="name" value={inputValue.name} onChange={handleChange} />
         </label><br />
 
         <label htmlFor="email">Email: 
-          <input type="email" id="email" onChange={handleChange} />
+          <input type="email" name="email" id="email" value={inputValue.email} onChange={handleChange} />
         </label><br />
 
         <label htmlFor="url">Image url: 
-          <input type="text" id="url" />
+          <input type="text" name="url" id="url" value={inputValue.url} onChange={handleChange} />
         </label><br />
 
         <label htmlFor="age">Edad: 
-          <input type="number" id="age" />
+          <input type="number" name="age" id="age" value={inputValue.age} onChange={handleChange} />
         </label><br />
         <button onClick={handleClick}>Enviar</button>
       </form>
--- src/Head/Head.jsx
+++ src/Head/Head.jsx
@@ -1,14 +1,9 @@
 import { useState } from 'react';
-import Card from './Card/Card';
-import Form from './Form/Form';
+import Card from './Card';
+import Form from './Form';
 
 const Head = () => {
-  const [data, setData] = useState({
-    name: "Marian",
-    email: "gromarant@gmail.com",
-    url: "https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_1280.jpg",
-    age: 32,
-  });
+  const [data, setData] = useState({});
 
   return (
     <>
